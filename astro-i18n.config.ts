57bffb78c18e4083d99712282bbdf96b9923cc88
Federico Nellen <nellenfederico@gmail.com>
2023-11-27 21:06:58 +0100

    Add Astro i18n configuration and update package.json

---
+++ astro-i18n.config.ts
@@ -0,0 +1,23 @@
+import { defineAstroI18nConfig } from "astro-i18n"
+
+export default defineAstroI18nConfig({
+	primaryLocale: "de", // default app locale
+	secondaryLocales: ["it", "en"], // other supported locales
+	fallbackLocale: "de", // fallback locale (on missing translation)
+	trailingSlash: "never", // "never" or "always"
+	run: "server", // "client+server" or "server"
+	showPrimaryLocale: false, // "/en/about" vs "/about"
+	translationLoadingRules: [], // per page group loading
+	translationDirectory: {}, // translation directory names
+	translations: {}, // { [translation_group1]: { [locale1]: {}, ... } }
+	routes: {
+		en: {
+			about: "/about",
+			home: "/",
+		},
+		it: {
+			about: "/it/about",
+			home: "/it",
+		},
+	}, // { [secondary_locale1]: { about: "about-translated", ... } }
+})
