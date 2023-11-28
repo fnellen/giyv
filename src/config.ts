15da1a4d2d268be0757ac2b23c59d76fedf0b141
Federico Nellen <nellenfederico@gmail.com>
2023-11-28 18:05:45 +0100

    Add global data to config.ts

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "German-Italian Young Voices"
+export const SITE_DESCRIPTION = "Empower change with German-Italian Young Voices. Engage in shaping the future of Europe through dynamic conversations. Join the community now!"
