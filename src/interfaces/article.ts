a8c2118c2565664e26f514edc9a8a71c296ddc1f
Federico Nellen <nellenfederico@gmail.com>
2023-11-28 18:08:16 +0100

    Add .prettierrc.cjs and update article interface

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -1,8 +1,6 @@
 import type Category from "./category";
 import type Image from "./image";
-/**
- * http://localhost:1337/api/articles?populate[featuredImage][fields][0]=name&populate[featuredImage][fields][1]=width&populate[featuredImage][fields][2]=height&populate[featuredImage][fields][3]=url&populate[featuredImage][fields][4]=alternativeText&populate[instagramPost][populate][images][fields][0]=url&populate[instagramPost][populate][images][fields][1]=name&populate[instagramPost][populate][images][fields][2]=width&populate[instagramPost][populate][images][fields][3]=height&populate[instagramPost][populate][images][fields][4]=alternativeText&populate[category]=*&populate[group][populate][authors][populate][bioImage][fields][0]=url&populate[group][populate][authors][populate][bioImage][fields][1]=name&populate[group][populate][authors][populate][bioImage][fields][2]=width&populate[group][populate][authors][populate][bioImage][fields][3]=height&populate[group][populate][authors][populate][bioImage][fields][4]=alternativeText
- */
+import type Node from "@strapi/blocks-react-renderer";
 
 export default interface Article {
     id: number;
@@ -12,17 +10,7 @@ export default interface Article {
         updatedAt: string;
         publishedAt: string;
         locale: string;
-        content: [
-            {
-                type: string;
-                level: number;
-                children: [
-                    {
-                        text: string;
-                    }
-                ];
-            }
-        ];
+        content: string;
         excerpt: string;
         slug: string;
         contentId: string;
