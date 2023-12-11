8d09dd0412e6ed3a76f86dddd6048b4930c11698
Federico Nellen <nellenfederico@gmail.com>
2023-12-11 09:59:51 +0100

    Refactor RSS page to fetch articles from Strapi API

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,23 +1,29 @@
 import rss from "@astrojs/rss";
 import sanitizeHtml from "sanitize-html";
-//import MarkdownIt from "markdown-it";
+import MarkdownIt from "markdown-it";
 import { l } from "astro-i18n";
 import type { APIRoute } from "astro";
-//const parser = new MarkdownIt();
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
+import fetchApi from "../lib/strapi";
+import type Article from "../interfaces/article";
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ request }) => {
-  const articles = [];
+  const articles = await fetchApi<Article[]>({
+    endpoint: "articles",
+    wrappedByKey: "data",
+  });
   return rss({
-    title: "Federico Nellen's Blog",
-    description: "Personal blog with articles about web development and more.",
-    site: "https://www.federico-nellen.de",
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: SITE_URL,
     items: articles.map((post) => ({
-      title: post.attributes.Title,
+      title: post.attributes.title,
       pubDate: post.attributes.publishedAt
         ? new Date(post.attributes.publishedAt)
         : new Date(),
-      description: post.attributes.Description,
-      //content: sanitizeHtml(parser.render(post.attributes.Content)),
+      description: post.attributes.excerpt,
+      content: sanitizeHtml(parser.render(post.attributes.content)),
       link: l(`/blog/${post.attributes.slug}/`, undefined, {
         targetLocale: post.attributes.locale,
       }),
