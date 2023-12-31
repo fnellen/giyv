3a38d4ae89c919f6ab722af652889b5546a03942
Federico Nellen <nellenfederico@gmail.com>
2023-12-31 16:43:49 +0100

    translations

--- astro-i18n.config.ts
+++ astro-i18n.config.ts
@@ -9,7 +9,34 @@ export default defineAstroI18nConfig({
 	showPrimaryLocale: false, // "/en/about" vs "/about"
 	translationLoadingRules: [], // per page group loading
 	translationDirectory: {}, // translation directory names
-	translations: {}, // { [translation_group1]: { [locale1]: {}, ... } }
+	translations: {
+		common: {
+			de: {
+				"not_found": {
+					"404": "Leider nichts gefunden ... 404",
+					"desc_1": "Die Seite, die du suchst, existiert leider nicht. Kehre zum Blog zurück oder schaue dir die neuesten Beiträge an.",
+					"desc_2": "Wenn du schon dabei bist, folge uns doch auf Instagram!",
+					"desc_3": "Falls du einen Fehler gefunden hast, kannst du uns gerne eine Email schreiben.",
+				},
+			},
+			en: {
+				"not_found": {
+					"404": "Nothing found ... 404",
+					"desc_1": "The page you're looking for doesn't exist. Return to the blog or check out the latest posts.",
+					"desc_2": "While you're at it, follow us on Instagram!",
+					"desc_3": "If you found an error, feel free to send us an email.",
+				},
+			},
+			it: {
+				"not_found": {
+					"404": "Niente trovato ... 404",
+					"desc_1": "La pagina che stai cercando non esiste. Torna al blog o dai un'occhiata agli ultimi post.",
+					"desc_2": "Mentre ci sei, seguici su Instagram!",
+					"desc_3": "Se hai trovato un errore, non esitare a inviarci una email.",
+				},
+			},
+		},
+	}, // { [translation_group1]: { [locale1]: {}, ... } }
 	routes: {
 		en: {
 			about: "about",
