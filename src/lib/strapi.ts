98a4b946db1150b3fbeaa6b025c0c5298654cae0
Federico Nellen <nellenfederico@gmail.com>
2024-01-25 14:39:33 +0100

    Add environment variables for Strapi authentication

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -22,8 +22,8 @@ export default async function fetchApi<T>({
     if (endpoint.startsWith('/')) {
         endpoint = endpoint.slice(1);
     }
-    let bearer = "Bearer 711c8d9896d155f75a865b11785a70685d0daf8581543397cc58f3e9157bd7c1021771f807f6bea0f13c22776eef03fedb21f5c08cdf33192e36d0a34240df544407c755de12c3e5454131d49d9a4bfeaa3ac7c670fd5629f586a1e4e94979311d75c9f856345046a48addb5aee94472dc0de2d30495d7507341d4da0431e5d9";
-    const url = new URL(`https://cms.giyv.eu/api/${endpoint}`);
+    let bearer = "Bearer " + import.meta.env.STRAPI_TOKEN;
+    const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
 
     const res = await fetch(url.toString() + (query ? `?${query}` : ""), {
         method: "GET",
