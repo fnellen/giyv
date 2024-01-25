5c2e4e31517837d8d7a17c93e7e74168580ae06e
Federico Nellen <nellenfederico@gmail.com>
2024-01-25 11:55:02 +0100

    Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import compressor from "astro-compressor";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor()],
+  integrations: [tailwind(), compressor(), purgecss()],
   site: "https://www.giyv.eu",
   output: "static",
   image: {
