4c66b956d620da9d798c9de77a4e634f5277dd02
Federico Nellen <nellenfederico@gmail.com>
2024-01-25 11:34:13 +0100

    Add astro-compressor integration to astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compressor()],
   site: "https://www.giyv.eu",
   output: "static",
   image: {
     domains: ["cms.giyv.eu", "storage.googleapis.com"],
-    remotePatterns: [{ protocol: "https" }],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   redirects: {
     "/junges-manifest": "/blog/junges-manifest",
-    "/manifesto-giovani": "/it/blog/manifesto-dei-giovani",
-  },
+    "/manifesto-giovani": "/it/blog/manifesto-dei-giovani"
+  }
 });
