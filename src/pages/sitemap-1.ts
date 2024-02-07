567ae6f54cbb3c7505b74621eeb369b1b0608e76
Federico Nellen <nellenfederico@gmail.com>
2024-02-07 12:18:24 +0100

    Update sitemap-1.ts to include correct blog URLs for alternate locales

--- src/pages/sitemap-1.ts
+++ src/pages/sitemap-1.ts
@@ -62,9 +62,9 @@ export const GET: APIRoute = async () => {
             let xhtml = '';
             if (article.attributes.localizations.data) {
                 article.attributes.localizations.data.forEach((localization) => {
-                    xhtml += `<xhtml:link rel="alternate" hreflang="${localization.attributes.locale}" href="${SITE_URL}${l(`/blog/${localization.attributes.slug}/`, undefined, {
+                    xhtml += `<xhtml:link rel="alternate" hreflang="${localization.attributes.locale}" href="${SITE_URL}${l(`/blog`, undefined, {
                         targetLocale: localization.attributes.locale,
-                    })}"/>`;
+                    }) + localization.attributes.slug}"/>`;
                 });
             }
             // Add the lastmod
